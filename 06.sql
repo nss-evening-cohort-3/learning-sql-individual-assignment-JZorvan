c72af593b74a929f7d76534f47cd20dd424806e3
Janelle Zorvan <jzorvan@gmail.com>
2016-08-29 21:06:49 -0500

    Finished 06

---
+++ 06.sql
@@ -0,0 +1,7 @@
+-- 6. Provide a query showing the invoices of customers who are from Brazil.
+
+SELECT Customer.FirstName, Customer.LastName, Invoice.*
+FROM Customer
+INNER JOIN Invoice
+ON Customer.CustomerId = Invoice.CustomerId
+WHERE Customer.Country = 'Brazil';
