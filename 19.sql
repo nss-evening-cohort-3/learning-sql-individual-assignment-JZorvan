19af2a146044a990c2966f13edd1f0f88bd4b16e
Janelle Zorvan <jzorvan@gmail.com>
2016-08-31 21:15:10 -0500

    Finished 19 (Fuck using MAX)

---
+++ 19.sql
@@ -0,0 +1,11 @@
+-- 19. Which sales agent made the most in sales in 2009?
+-- Fuck MAX, man.  Fuck it.
+
+SELECT Employee.FirstName || ' ' || Employee.LastName AS 'Sales Rep' FROM Invoice
+JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+WHERE Invoice.InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31'
+GROUP BY Employee.LastName ORDER BY 'Total Sales' DESC LIMIT 1;
+
+
+
