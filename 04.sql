85863597e4aa79a4f62fc13339b4446ff7320cf4
Janelle Zorvan <jzorvan@gmail.com>
2016-09-03 10:39:48 -0500

    Added details

--- 04.sql
+++ 04.sql
@@ -1,3 +1,4 @@
 -- 4. Provide a query showing only the Employees who are Sales Agents.
 
-SELECT * FROM Employee WHERE Title = 'Sales Support Agent';
+SELECT LastName || ", " || FirstName AS 'Sales Agents' FROM Employee WHERE Title = 'Sales Support Agent';
+
