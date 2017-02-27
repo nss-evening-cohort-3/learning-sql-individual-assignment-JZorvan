e1b83ec9093534a3d4be9ce9293c61c136c7b539
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:36:25 -0600

    Delete 26.sql

--- 26.sql
+++
@@ -1,8 +0,0 @@
--- 26. Provide a query that shows the top 3 best selling artists.
-
-SELECT Artist.Name AS 'Top 3 Best-Selling Artists' FROM Artist
-JOIN Album ON Artist.ArtistId = Album.ArtistId
-JOIN Track ON Album.AlbumId = Track.AlbumId
-JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
-GROUP BY Artist.Name
-ORDER BY COUNT (InvoiceLine.TrackId) DESC LIMIT 3;
