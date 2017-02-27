b55f64496328e2eef5df7240276d17874c523b9d
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:36:15 -0600

    Delete 25.sql

--- 25.sql
+++
@@ -1,5 +0,0 @@
--- 25. Provide a query that shows the top 5 most purchased tracks over all.
-
-SELECT Track.Name AS 'Top Tracks' FROM Track
-JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
-GROUP BY Track.Name ORDER BY COUNT (InvoiceLine.TrackId) DESC LIMIT 5;
