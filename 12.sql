61d249a0fdfe51512e08fe4d3a52b1704c9b3756
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:30:51 -0600

    Delete 12.sql

--- 12.sql
+++
@@ -1,8 +0,0 @@
--- 12. Provide a query that includes the track name with each invoice line item.
-
-SELECT InvoiceLine.InvoiceLineId AS 'Invoice Line #', Track.Name AS 'Track Name'
-FROM InvoiceLine
-JOIN Track
-ON InvoiceLine.TrackId = Track.TrackId
-ORDER BY InvoiceLine.InvoiceLineId;
-
