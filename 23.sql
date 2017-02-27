b0b4285da30c378737ba7405ec1cd61d4b863a27
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:35:50 -0600

    Delete 23.sql

--- 23.sql
+++
@@ -1,5 +0,0 @@
--- 23. Provide a query that shows the total sales per country. Which country's customers spent the most?
-
-SELECT Customer.Country, (SELECT SUM (Invoice.Total)) AS 'Total Sales' FROM Invoice
-JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
-GROUP BY Customer.Country;
