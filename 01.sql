5415738314bb3ac748035135388f22ad605edb2a
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 10:39:17 -0600

    Delete 01.sql

--- 01.sql
+++
@@ -1,4 +0,0 @@
--- 1. Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
-
-
-SELECT CustomerId AS 'Customer Id#', Last Name || ', ' || FirstName AS 'Customer', Country AS 'Customer Country' FROM Customer WHERE Country != 'USA';
