b8dcec364df29b94bd0d6879714349e1550982ab
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:36:36 -0600

    Delete 27.sql

--- 27.sql
+++
@@ -1,7 +0,0 @@
--- 27. Provide a query that shows the most purchased Media Type.
-
-SELECT MediaType.Name AS 'Most Purchased Media Type' FROM MediaType
-JOIN Track ON MediaType.MediaTypeId = Track.MediaTypeId
-JOIN InvoiceLine ON Track.TrackId = InvoiceLine.TrackId
-GROUP BY MediaType.Name
-ORDER BY COUNT (InvoiceLine.TrackId) DESC LIMIT 1;
