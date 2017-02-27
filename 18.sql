479f2a2401e8bc05bcb496119cd1742a86620501
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:33:15 -0600

    Delete 18.sql

--- 18.sql
+++
@@ -1,6 +0,0 @@
--- 18. Provide a query that shows total sales made by each sales agent.
-
-SELECT Employee.FirstName || ' ' || Employee.LastName AS 'Sales Rep', (SELECT SUM (Invoice.Total)) AS 'Total Sales' FROM Invoice
-JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
-JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
-GROUP BY Employee.LastName;
