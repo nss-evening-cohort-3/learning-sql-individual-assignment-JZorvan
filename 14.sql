cb3b9ee8d77563fc7a8e4088e9cd30cff23f6b94
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:31:26 -0600

    Delete 14.sql

--- 14.sql
+++
@@ -1,5 +0,0 @@
--- 14. Provide a query that shows the # of invoices per country.
-
-SELECT BillingCountry AS 'Country', (SELECT COUNT (BillingCountry)) AS '# of Invoices'
-FROM Invoice
-GROUP BY BillingCountry;
