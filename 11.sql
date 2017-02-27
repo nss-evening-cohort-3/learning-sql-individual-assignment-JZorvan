a13ae385561dae2c731a4d1051a24ee30f8a2b1c
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 11:30:45 -0600

    Delete 11.sql

--- 11.sql
+++
@@ -1,5 +0,0 @@
--- 1. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice.
-
-SELECT InvoiceId AS 'Invoice ID#', (SELECT COUNT (InvoiceLineId)) AS 'Number of Line Items'
-FROM InvoiceLine
-GROUP BY InvoiceId;
