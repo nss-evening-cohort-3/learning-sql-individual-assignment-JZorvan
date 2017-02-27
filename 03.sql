4203ef666e46640dc1600961af5631aa531182c0
Janelle Zorvan <jzorvan@gmail.com>
2017-02-27 10:39:41 -0600

    Delete 03.sql

--- 03.sql
+++
@@ -1,7 +0,0 @@
--- 3. Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
-
-SELECT Customer.FirstName || ' ' || Customer.LastName AS Customer, Invoice.InvoiceId AS 'Invoice ID', Invoice.InvoiceDate AS 'Invoice Date', Invoice.BillingCountry AS 'Billing Country'
-FROM Customer
-INNER JOIN Invoice
-ON Customer.CustomerId = Invoice.CustomerId
-WHERE Customer.Country = 'Brazil';
